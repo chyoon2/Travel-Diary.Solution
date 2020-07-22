ab9309d5f35ccee86bceea3a74b62bbc798c3b73
Chris Yoon <chy.yoon@gmail.com>
2020-07-22 11:03:47 -0700

    add views code
    Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com>

--- Places/Controllers/HomeController.cs
+++ Places/Controllers/HomeController.cs
@@ -10,6 +10,5 @@ namespace Places.Controllers
     {
       return View();
     }
-
   }
 }
--- Places/Controllers/LocationsController.cs
+++ Places/Controllers/LocationsController.cs
@@ -1,9 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Places.Models;
+using System.Collections.Generic;
 
 namespace Places.Controllers
 {
-  public class HomeController : Controller
+  public class LocationsController : Controller
   {
-    
+    [HttpGet("/locations")]
+    public ActionResult Index()
+    {
+      List<Location> allPlaces = Location.GetAll();
+      return View(allPlaces);
+    }
+
+    [HttpGet("/locations/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/locations")]
+    public ActionResult Create(string cityName)
+    {
+      Location myLocation = new Location (cityName);
+      return RedirectToAction("Index");
+    }
   }
 }
